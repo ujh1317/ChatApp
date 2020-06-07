06ba14e94ba5e3d1d96808317f23880ba3cf3a2a
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-07 17:31:31 +0900

    6/07 add Chat model

--- app/src/main/java/com/example/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -4,22 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Comment;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private static final String TAG = "ChatActivity";
     private RecyclerView recyclerView;
     MyAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-
+    EditText etText;
+    Button btnSend;
+    String stEmail;
+    FirebaseDatabase database;
+    ArrayList<Chat> chatArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+
+        chatArrayList = new ArrayList<>();
+        stEmail = getIntent().getStringExtra("email");
         Button btnFinish = (Button) findViewById(R.id.btnFinish);
         btnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +54,9 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        etText = (EditText) findViewById(R.id.etText);
 
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
@@ -40,7 +70,92 @@ public class ChatActivity extends AppCompatActivity {
 
         // specify an adapter (see also next example)
         String[] myDataset = {"test1", "test2", "test3", "test4"};
-        mAdapter = new MyAdapter(myDataset);
+        mAdapter = new MyAdapter(chatArrayList);
         recyclerView.setAdapter(mAdapter);
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                // A new comment has been added, add it to the displayed list
+                Chat chat = dataSnapshot.getValue(Chat.class);
+                String commentKey = dataSnapshot.getKey();
+                String stEmail = chat.getEmail();
+                String stText = chat.getText();
+                Log.d(TAG, "stEmail: "+stEmail);
+                Log.d(TAG, "stText: "+stText);
+                chatArrayList.add(chat);
+                mAdapter.notifyDataSetChanged();
+                // ...
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+
+
+
+                // ...
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                String commentKey = dataSnapshot.getKey();
+
+                // ...
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                Toast.makeText(ChatActivity.this, "Failed to load comments.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        DatabaseReference ref = database.getReference("message");
+        ref.addChildEventListener(childEventListener);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stText = etText.getText().toString();
+                Toast.makeText(ChatActivity.this, "MSG : "+stText, Toast.LENGTH_LONG).show();
+
+
+
+                // 날짜별로 데이터 쌓기
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String datetime = dateFormat.format(c.getTime());
+
+                DatabaseReference myRef = database.getReference("message").child(datetime);
+
+                // email, text 구분해서 데이터 받기
+                Hashtable<String, String> numbers
+                        = new Hashtable<String, String>();
+                numbers.put("email", stEmail);
+                numbers.put("text", stText);
+
+                myRef.setValue(numbers);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     EditText etId, etPassword;
     private FirebaseAuth mAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +35,53 @@ public class MainActivity extends AppCompatActivity {
 
         etId = (EditText) findViewById(R.id.etId);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_LONG).show();
-                Intent in = new Intent(MainActivity.this, ChatActivity.class);
-                startActivity(in);
+                final String stEmail = etId.getText().toString();
+                String stPassword = etPassword.getText().toString();
+                if (stEmail.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please insert Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (stPassword.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // 로그인 버튼이 눌렸을 때 로딩화면 띄우기 - 6/07
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(stEmail, stPassword)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    String stUserEmail = user.getEmail();
+                                    String stUserName = user.getDisplayName();
+                                    Log.d(TAG, "stUserEmail : " +stUserEmail+", stUserName : " +stUserName);
+                                    Intent in = new Intent(MainActivity.this, ChatActivity.class);
+                                    in.putExtra("email", stEmail);
+                                    startActivity(in);
+//                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                                    updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
+
             }
         });
 
@@ -58,11 +99,13 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Please insert Password", Toast.LENGTH_LONG).show();
                     return;
                 }
+                progressBar.setVisibility(View.VISIBLE);
 
                 mAuth.createUserWithEmailAndPassword(stEmail, stPassword)
                         .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
