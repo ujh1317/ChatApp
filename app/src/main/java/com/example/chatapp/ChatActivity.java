c18402cc3ef4114e2c01a25d18c572eb8f4c65af
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-07 14:14:32 +0900

    6/07 recyclerview

--- app/src/main/java/com/example/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -1,12 +1,19 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,20 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"test1", "test2", "test3", "test4"};
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
